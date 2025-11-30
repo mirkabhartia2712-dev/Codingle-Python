263d8379f94b742b5ec204ba467e849ab73698a9
mirkabhartia2712-dev <mirka.bhartia2712@gmail.com>
2025-11-30 11:42:09 +0530

    Initial Commit

--- Lesson 2/Activity.py
+++ Lesson 2/Activity.py
@@ -1,6 +1,6 @@
 print("Hello\n" ,end= "my name is mirka\n")
 a= input("Enter any age number")
 print(a)
-print("I", "like", "to", "swim", sep="--")
+print("I", "like", "to", "go home
+", sep="--")
 import keyword
-print(keyword.kwlist)
---
+++ Lesson 3/Activity1.py
@@ -0,0 +1,14 @@
+x=14
+print (x,type(x))
+y=3.141592693985
+print (y,type(y))
+z=("a")
+print (z,type(z))
+j=False
+print (j,type(j))
+i=float(x)
+print(i,type(i))
+k=str(y)
+print(k,type(k))
+h=int(y)
+print (h,type(h))
