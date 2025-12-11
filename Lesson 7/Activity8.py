b6ba2390b01dc2dbe058adf7dbde8a5fb69ab1f7
mirkabhartia2712-dev <mirka.bhartia2712@gmail.com>
2025-12-11 18:58:12 +0530

    Initial Commit

---
+++ Lesson 7/Activity8.py
@@ -0,0 +1,16 @@
+x=14
+if type(x)is int:
+    print ("x is an integer")
+else:
+    print ("x is not an integer")
+y=16.01
+if type(y)is float:
+    print ("y is an float")
+else:
+    print ("y is not an float")
+z=x
+if x is z:
+    print ("x is equal to z")
+else:
+    print ("x is not equal to z")
+
