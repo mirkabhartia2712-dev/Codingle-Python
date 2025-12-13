62b467e3c0c828f406c28b9822f874817e2f37fb
mirkabhartia2712-dev <mirka.bhartia2712@gmail.com>
2025-12-13 20:49:11 +0530

    Initial Commit

---
+++ Homework projects/hwproject1.py
@@ -0,0 +1,8 @@
+ch = input("Enter one character: ")
+
+if ch >= 'A' and ch <= 'Z':
+    print("It is an alphabet.")
+elif ch >= 'a' and ch <= 'z':
+    print("It is an alphabet.")
+else:
+    print("It is not an alphabet.")
