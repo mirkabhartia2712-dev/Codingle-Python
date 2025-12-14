f5d47ca96167f5ca8601c53bf9d600aa5848c024
mirkabhartia2712-dev <mirka.bhartia2712@gmail.com>
2025-12-14 11:27:08 +0530

    Initial Commit

---
+++ Lesson 8/Activity 1.py
@@ -0,0 +1,6 @@
+x=int(input("Enter a number as the numerator"))
+y=int(input("Enter a number as the denominator"))
+if x%y==0:
+    print ("The numerator is devisable by the denominator")
+else:
+    print("The numerator is not devisable by the denominator")
