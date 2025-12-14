64cba34dd579b274fffa39f4341f1d28cdcff2e9
mirkabhartia2712-dev <mirka.bhartia2712@gmail.com>
2025-12-14 18:25:43 +0530

    Initial Commit

---
+++ Homework projects/hwproject2.py
@@ -0,0 +1,18 @@
+x = 4
+y = 6
+z = 9
+
+print("Before swapping:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+t = x
+x = y
+y = z
+z = t
+
+print("After swapping:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
