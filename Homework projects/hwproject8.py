4db6a60b7463b73b6d561816a1c2864f6b20948c
mirkabhartia2712-dev <mirka.bhartia2712@gmail.com>
2025-12-22 11:06:07 +0530

    Initial Commit

---
+++ Homework projects/hwproject8.py
@@ -0,0 +1,20 @@
+friend1 = input("Enter friend 1 name: ")
+birthday1 = input("Enter friend 1 birthday: ")
+
+friend2 = input("Enter friend 2 name: ")
+birthday2 = input("Enter friend 2 birthday: ")
+
+friend3 = input("Enter friend 3 name: ")
+birthday3 = input("Enter friend 3 birthday: ")
+
+friend4 = input("Enter friend 4 name: ")
+birthday4 = input("Enter friend 4 birthday: ")
+
+friend5 = input("Enter friend 5 name: ")
+birthday5 = input("Enter friend 5 birthday: ")
+
+print(friend1, "-", birthday1)
+print(friend2, "-", birthday2)
+print(friend3, "-", birthday3)
+print(friend4, "-", birthday4)
+print(friend5, "-", birthday5)
