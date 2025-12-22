664f2d4065fcd7db81cde871ef4592d91963775d
mirkabhartia2712-dev <mirka.bhartia2712@gmail.com>
2025-12-22 10:41:49 +0530

    Initial Commit

---
+++ Homework projects/hwproject5.py
@@ -0,0 +1,9 @@
+temperature = float(input("Enter the current temperature in Celsius: "))
+
+if temperature >= 25:
+    print("The temperature is suitable for wearing light clothes.")
+elif temperature >= 20:
+    print("The temperature is moderate. Light clothes might be okay.")
+else:
+    print("The temperature is too cold for light clothes. Wear warm clothing.")
+
