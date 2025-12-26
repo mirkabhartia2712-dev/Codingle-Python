d5555827fc8a118976719cf6932e9dadb3006ded
mirkabhartia2712-dev <mirka.bhartia2712@gmail.com>
2025-12-26 12:16:11 +0530

    Initial Commit

---
+++ Rule based chatbots/Activity1.py
@@ -0,0 +1,13 @@
+import re, random
+from colorama import Fore, init
+init (autoreset=True)
+destinations={
+    
+    "beaches": ["Bali", "Maldives", "Phuket"],
+
+    "mountains": ["Swiss Alps", "Rocky Mountains", "Himalayas"],
+
+    "cities": ["Tokyo", "Paris", "New York"]
+
+    
+}
